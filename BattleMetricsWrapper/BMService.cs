1baf219e168158b09ec85a15821daa574b084c36
BonkiTheDane <asger-gjelstrup@hotmail.com>
2020-07-25 12:28:43 +0200

    Revert "Update BMService.cs"
    This reverts commit 0653364bdb5fcd23b3c2d7dea954ef0cc0726cef, reversing
    changes made to 2cb22e17878d1bac60e68404cb43cc531ce28567.

--- BattleMetricsWrapper/BMService.cs
+++ BattleMetricsWrapper/BMService.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -7,7 +8,6 @@ namespace BattleMetricsWrapper
 {
     public class BMService
     {
-<<<<<<< HEAD
         private HttpClient bmClient = new HttpClient();
 
         public async Task<Server> GetBMServerAsync(int serverId)
@@ -18,22 +18,6 @@ namespace BattleMetricsWrapper
                 var json = await result.Content.ReadAsStringAsync();
                 var server = JsonConvert.DeserializeObject<BattleMetricsResponse>(json).Server;
                 return server;
-=======
-        private static HttpClient HttpClient { get; set; }
-
-        public BMService()
-        {
-            BMService.HttpClient = new HttpClient();
-            BMService.HttpClient.DefaultRequestHeaders.ConnectionClose = true;
-        }
-
-        public async Task<Server> GetBMServer(UInt32 ServerID)
-        {
-            try
-            {
-                var response = await HttpClient.GetAsync($"https://api.battlemetrics.com/servers/{ServerID}?include=session");
-                return JsonConvert.DeserializeObject<BattleMetricsResponse>(await response.Content.ReadAsStringAsync()).Server;
->>>>>>> pr/1
             }
             catch (Exception) { return null; }
         }
