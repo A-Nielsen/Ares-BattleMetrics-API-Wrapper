0ec5a0706602e28dc399ae04e484582efdbb8d7c
BloodfallenTear <bloodfallentear@hotmail.com>
2020-07-25 12:19:37 +0200

    Update BMService.cs

--- BattleMetricsWrapper/BMService.cs
+++ BattleMetricsWrapper/BMService.cs
@@ -1,26 +1,28 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BattleMetricsWrapper
 {
     public class BMService
     {
+        private static HttpClient HttpClient { get; set; }
 
-        public async Task<Server> GetBMServer(int serverId)
+        public BMService()
+        {
+            BMService.HttpClient = new HttpClient();
+            BMService.HttpClient.DefaultRequestHeaders.ConnectionClose = true;
+        }
+
+        public async Task<Server> GetBMServer(UInt32 ServerID)
         {
             try
             {
-                var bmClient = new HttpClient();
-                var result = await bmClient.GetAsync($"https://api.battlemetrics.com/servers/{serverId}?include=session");
-                string json = await result.Content.ReadAsStringAsync();
-                var server = JsonConvert.DeserializeObject<BattleMetricsResponse>(json).Server;
-                return server;
+                var response = await HttpClient.GetAsync($"https://api.battlemetrics.com/servers/{ServerID}?include=session");
+                return JsonConvert.DeserializeObject<BattleMetricsResponse>(await response.Content.ReadAsStringAsync()).Server;
             }
-            catch (Exception ex) { return null; }
+            catch (Exception) { return null; }
         }
     }
 }
